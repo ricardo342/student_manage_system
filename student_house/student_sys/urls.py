bf0bfd9836bce05330a9b4160a1423731153e1c6
majiaqin <majiaqin@techb.top>
2022-11-09 09:56:14 +0800

    修复django2.0版本后不适用^,$作路由的问题

--- student_house/student_sys/urls.py
+++ student_house/student_sys/urls.py
@@ -21,6 +21,6 @@ sys.path.append(os.path.dirname(sys.path[0]))
 from student_house.student.views import index
 
 urlpatterns = [
-    path(r'^$', index, name='index'),
+    path('index/', index, name='index'),
     path('admin/', admin.site.urls),
 ]
