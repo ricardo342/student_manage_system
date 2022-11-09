f3572840578490292a40361f47d961127e4b6f79
majiaqin <majiaqin@techb.top>
2022-11-09 11:33:11 +0800

    修复404问题

--- student_house/student/views.py
+++ student_house/student/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def index(request):
     words = 'World!'
-    return render(request, 'index.html', context={'words', words})
+    return render(request, 'index.html', context={'words': words})
